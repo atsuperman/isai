10bfe4387eaaf863a4f6a2a646f34817ac2ad826
atsuperman <smstarvinoth@gmail.com>
2026-04-10 14:53:22 +0530

    Update isai.js

--- isai.js
+++ isai.js
@@ -3,6 +3,7 @@ const repos = [
   {
     base: "https://github.com/atsuperman/play2/raw/main/",
     songs: [
+{ file: "Vetri Vetri Endru sollum.m4a", details: "🎬 Kattumarakaran (1995) / 🎙️ S. P. Balasubrahmanyam, K. S. Chithra / 🎼 Ilaiyaraaja / 📝 Vaali", duration: 331 },
 { file: "Vacha Parvai Theerathadi.m4a", details: "🎬 Ilamai Kolam (1980) / 🎙️ K. J. Yesudas / 🎼 Ilaiyaraaja / 📝 Gangai Amaran", duration: 234 },  
 { file: "Chikkimukki Kallu.m4a", details: "🎬 Citizen (2001) / 🎙️ Shankar Mahadevan, Sadhana Sargam / 🎼 Deva / 📝 Vairamuthu", duration: 411 },
 { file: "I Like You.m4a", details: "🎬 Citizen (2001) / 🎙️ Vasundhara Das / 🎼 Deva / 📝 Vairamuthu", duration: 412 },
