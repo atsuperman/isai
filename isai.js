66e554c548ce3123511b952d7b65ce5c4b6958c3
atsuperman <smstarvinoth@gmail.com>
2026-05-10 19:08:46 +0530

    Update isai.js

--- isai.js
+++ isai.js
@@ -3,6 +3,7 @@ const repos = [
   {
     base: "https://github.com/atsuperman/play2/raw/main/",
     songs: [
+{ file: "Thalapathy Kacheri.m4a", details: "🎬 Jana Nayagan (2026) / 🎙️ Anirudh Ravichander, Thalapathy Vijay, Arivu / 🎼 Anirudh Ravichander / 📝 Arivu", duration: 197 },
 { file: "Saami Saami.m4a", details: "🎬 Pushpa The Rise (2021) / 🎙️ Rajalakshmi Senthil ganesh / 🎼 Devi Sri Prasad / 📝 Viveka", duration: 224 },
 { file: "Oo Solriya Oo Oo Solriya.m4a", details: "🎬 Pushpa The Rise (2021) / 🎙️ Andrea Jeremiah / 🎼 Devi Sri Prasad / 📝 Viveka", duration: 223 },
 { file: "Srivalli.m4a", details: "🎬 Pushpa The Rise (2021) / 🎙️ Sid Sriram / 🎼 Devi Sri Prasad / 📝 Chandrabose", duration: 221 },
